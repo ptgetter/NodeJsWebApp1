585f9c7352f5ed6382ed5b4186c77e15891501d2
ptgetter <ptgetter@nate.com>
2017-09-15 16:08:31 +0900

    commit test

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -4,5 +4,6 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('test');
     res.end('Hello World\n');
 }).listen(port);
